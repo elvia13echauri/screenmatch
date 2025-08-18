a07215b1d40005e1526a0999f46d969ba628019b
ElviaEchauri <echaur.ely@gmail.com>
2025-08-17 18:57:54 -0600

    update Categoria enum and Serie class

--- src/main/java/com/aluracursos/screenmatch/model/Serie.java
+++ src/main/java/com/aluracursos/screenmatch/model/Serie.java
@@ -2,6 +2,8 @@ package com.aluracursos.screenmatch.model;
 
 import com.fasterxml.jackson.annotation.JsonAlias;
 
+import java.util.OptionalDouble;
+
 public class Serie {
     private String titulo;
     private Integer totalDeTemporadas;
@@ -10,4 +12,81 @@ public class Serie {
     private String actores;
     private String poster;
     private String sinopsis;
+
+    public Serie(DatosSerie datosSerie){
+        this.titulo = datosSerie.titulo();
+        this.totalDeTemporadas = datosSerie.totalDeTemporadas();
+        this.evaluacion = OptionalDouble.of(Double.valueOf(datosSerie.evaluacion())).orElse(0);
+        this.poster = datosSerie.poster();
+        this.genero = Categoria.fromString(datosSerie.genero().split(",")[0].trim());
+        this.actores = datosSerie.actores();
+        this.sinopsis = datosSerie.sinopsis();
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Integer getTotalDeTemporadas() {
+        return totalDeTemporadas;
+    }
+
+    public void setTotalDeTemporadas(Integer totalDeTemporadas) {
+        this.totalDeTemporadas = totalDeTemporadas;
+    }
+
+    public Double getEvaluacion() {
+        return evaluacion;
+    }
+
+    public void setEvaluacion(Double evaluacion) {
+        this.evaluacion = evaluacion;
+    }
+
+    public Categoria getGenero() {
+        return genero;
+    }
+
+    public void setGenero(Categoria genero) {
+        this.genero = genero;
+    }
+
+    public String getActores() {
+        return actores;
+    }
+
+    public void setActores(String actores) {
+        this.actores = actores;
+    }
+
+    public String getPoster() {
+        return poster;
+    }
+
+    public void setPoster(String poster) {
+        this.poster = poster;
+    }
+
+    public String getSinopsis() {
+        return sinopsis;
+    }
+
+    public void setSinopsis(String sinopsis) {
+        this.sinopsis = sinopsis;
+    }
+
+    @Override
+    public String toString() {
+        return "titulo='" + titulo + '\'' +
+                ", totalDeTemporadas=" + totalDeTemporadas +
+                ", evaluacion=" + evaluacion +
+                ", genero=" + genero +
+                ", actores='" + actores + '\'' +
+                ", poster='" + poster + '\'' +
+                ", sinopsis='" + sinopsis + '\'';
+    }
 }
